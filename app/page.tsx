19aae00bd3c9110f7d30477edaf77a0c2b0d7e96
Ben Kvale <benkvale@Bens-MacBook-Pro-2.local>
2025-11-24 16:33:47 -0600

    Remove StudioSection from page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/Hero';
 import WorkSection from '@/components/WorkSection';
-import StudioSection from '@/components/StudioSection';
 import LabSection from '@/components/LabSection';
 import AboutSection from '@/components/AboutSection';
 import ContactSection from '@/components/ContactSection';
@@ -10,7 +9,6 @@ export default function Page() {
     <main className="relative z-10 flex flex-col gap-12">
       <Hero />
       <WorkSection />
-      <StudioSection />
       <LabSection />
       <AboutSection />
       <ContactSection />
